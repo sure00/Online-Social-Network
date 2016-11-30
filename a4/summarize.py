5702ec1240d4a76d3895116798d8d569d48aa2b3
sure00 <syan9@hawk.iit.edu>
2016-11-30 09:48:11 -0600

    remove logs

--- a4/summarize.py
+++ a4/summarize.py
@@ -35,7 +35,6 @@ def loadData(filename):
         except EOFError:
             return {}
 
-    #print("Load %d  data "%len(data))
     return data
 
 # append to database
@@ -49,13 +48,11 @@ def main():
     summary=[]
 
     userInfo = loadData(userFile)
-    #print(userInfo)
+
     total = 0
     for u in userInfo.values():
         if u == []:
             continue
-        #print(u)
-        #print(len(u))
         total += len(u)
 
     summary.append('Number of users collected: '+ str(total))
@@ -72,26 +69,17 @@ def main():
     summary.append('Average number of users per community:' + str(total/len(components)))
 
     classify = loadData(classifyDataFile)
-   # print(classify)
-    #print(classify['Sentiment'])
+
     summary.append('\nNumber of instances per class found: ')
     summary.append('\tAganist Trump Tweets: ' + str(len(classify['Againist_idx'])))
     summary.append('\tNeutral Trump Tweets: ' + str(len(classify['Neutral_idx'])))
     summary.append('\tSupport Trump Tweets: ' + str(len(classify['Support_idx'])))
 
     summary.append('\nNumber of instances per class found:  ')
-    #print(tweets[2])
-    #print(classify['Againist_idx'][2])
-
-    #print(tweets[2]['text'])
 
     summary.append('\t\nInstance of Aganist Trump Tweets: ' + str(tweets[(classify['Againist_idx'])[0]]['text']))
     summary.append('\t\nInstance of Neutral Trump Tweets: ' + str(tweets[(classify['Neutral_idx'])[0]]['text']))
     summary.append('\t\nInstance of Support Trump Tweets: ' + str(tweets[(classify['Support_idx'])[0]]['text']))
-
-    #print([(classify['Againist_idx'])[1]])
-    #print([(classify['Neutral_idx'])[1]])
-    #print([(classify['Support_idx'])[1]])
 
     SupportGender = Counter(classify['SupportGender'])
     NeutralGender= Counter(classify['NeutralGender'])
@@ -100,12 +88,9 @@ def main():
     summary.append('\n\nThere are ' + str(SupportGender[1]/sum(SupportGender.values())) + '% femal Support Trump and '
                    + str(SupportGender[0]/sum(SupportGender.values())) + '% male')
 
-    #print("Againist_idx",classify['Againist_idx'])
     getFemal =getMale=0
     for index in range(len(classify['SupportGender'])):
         if getFemal == 1 and getMale ==1:
-            #print(tweets[classify['Againist_idx'][femaleNamesID]]['user']['name'])
-            #print(tweets[classify['Againist_idx'][maleNameId]]['user']['name'])
 
             summary.append('In Support Trump tweets, one female name is :' +
                            str(tweets[classify['Againist_idx'][femaleNamesID]]['user']['name']) + '\t male name :'
@@ -122,12 +107,9 @@ def main():
     summary.append('\t\nThere are ' + str(NeutralGender[1]/sum(NeutralGender.values())) + '% femal neutral Trump and '
                    + str(NeutralGender[0]/sum(NeutralGender.values())) + '% male')
 
-    #print("Againist_idx",classify['Againist_idx'])
     getFemal =getMale=0
     for index in range(len(classify['NeutralGender'])):
         if getFemal == 1 and getMale ==1:
-            #print(tweets[classify['Neutral_idx'][femaleNamesID]]['user']['name'])
-            #print(tweets[classify['Neutral_idx'][maleNameId]]['user']['name'])
 
             summary.append('In Neutral Trump tweets, one female name is :' +
                            str(tweets[classify['Neutral_idx'][femaleNamesID]]['user']['name']) + '\t male name :'
@@ -146,8 +128,6 @@ def main():
     getFemal =getMale=0
     for index in range(len(classify['AgainistGender'])):
         if getFemal == 1 and getMale ==1:
-            #print(tweets[classify['Againist_idx'][femaleNamesID]]['user']['name'])
-            #print(tweets[classify['Againist_idx'][maleNameId]]['user']['name'])
 
             summary.append('In Againist Trump tweets, one female name is :' +
                            str(tweets[classify['Againist_idx'][femaleNamesID]]['user']['name']) + '\t male name :'
@@ -162,7 +142,6 @@ def main():
 
     saveData(summary,summaryFile)
     print("Summarize Done")
-
 
 if __name__ == '__main__':
     main()
